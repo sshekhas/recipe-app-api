a609ac5a0d8cd10bdd61d3b73ac83668fcaf9e75
sshekhas <18996ssp@gmail.com>
2020-09-26 15:34:42 +0530

    added create tad function

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,8 +8,8 @@ from recipe import serializers
 
 
 class TagViewSet(viewsets.GenericViewSet,
-                    mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
